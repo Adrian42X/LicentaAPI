e77ae2c63716a0efc46a384f38692ae2da004d52
Adrian42X <89139357+Adrian42X@users.noreply.github.com>
2023-04-09 22:06:01 +0300

    Update user entity and add repository pattern

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -32,9 +32,9 @@ namespace API.Entities
 
         public List<Photo> Photos { get; set; } = new();
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        //public int GetAge()
+        //{
+        //    return DateOfBirth.CalculateAge();
+        //}
     }
 }
